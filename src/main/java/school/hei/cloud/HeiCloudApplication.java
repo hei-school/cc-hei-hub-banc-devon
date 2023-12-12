254c65b6a99ed9c95a7c554dc7136249a7d92e81
Onitsiky <hei.onitsiky@gmail.com>
2023-12-12 22:09:00 +0300

    feat: initialize spring boot project

---
+++ src/main/java/school/hei/cloud/HeiCloudApplication.java
@@ -0,0 +1,13 @@
+package school.hei.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeiCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeiCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/school/hei/cloud/HeiCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package school.hei.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeiCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
