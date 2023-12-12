ee9241325fc64e6cf9527a2edd57cfa66dcf6e8a
dinasoa <hei.dinasoa@gmail.com>
2023-12-12 23:25:29 +0300

    chore: handle error with try/except

--- upload_files.py
+++ upload_files.py
@@ -20,33 +20,39 @@ def allowed_file(filename, allowed_extensions):
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    if 'filename' not in request.files or 'folder' not in request.form:
-        return jsonify({'error': 'No file or type provided. '}), 400
+    try:
+        if 'filename' not in request.files or 'folder' not in request.form:
+            raise ValueError('No file or type provided.')
 
-    file = request.files['filename']
-    file_type = request.form['folder'].lower()
+        file = request.files['filename']
+        file_type = request.form['folder'].lower()
 
-    allowed_extensions = {
-        'images': {'jpg', 'jpeg', 'png'},
-        'videos': {'mp4', 'avi', 'mov', 'mkv'},
-        'pdfs': {'pdf'},
-        'docs': {'docx'}
-    }
+        allowed_extensions = {
+            'images': {'jpg', 'jpeg', 'png'},
+            'videos': {'mp4', 'avi', 'mov', 'mkv'},
+            'pdfs': {'pdf'},
+            'docs': {'docx'}
+        }
 
-    if file_type not in UPLOAD_FOLDER:
-        return jsonify({'error': 'Invalid file type.'}), 400
+        if file_type not in UPLOAD_FOLDER:
+            raise ValueError('Invalid file type, should be: images/videos/pdfs/docs.')
 
-    if not allowed_file(file.filename, allowed_extensions[file_type]):
-        return jsonify({'error': 'Unsupported media type. '}), 415
+        if not allowed_file(file.filename, allowed_extensions[file_type]):
+            raise ValueError('Unsupported media type.')
 
-    filepath = os.path.join(app.config['UPLOAD_FOLDER'][file_type], file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'][file_type], file.filename)
 
-    if os.path.exists(filepath):
-        return jsonify({'error': 'Filename should be unique.'}), 400
+        if os.path.exists(filepath):
+            raise ValueError('Filename should be unique.')
 
-    file.save(filepath)
+        file.save(filepath)
 
-    return jsonify({'success': f'{file_type} uploaded.'}), 200
+        return jsonify({'success': f'{file_type} uploaded.'}), 200
+
+    except ValueError as e:
+        if str(e) == "Unsupported media type.":
+            return jsonify({'error': str(e)}), 415
+        return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
